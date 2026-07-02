83b24a9d158b23c9d19c0d809037dc96782950b7
Bambang Purnomosid D. P. <bpurnomo@akakom.ac.id>
2026-07-02 14:37:57 +0700

    commit all

--- database/migrations/2026_04_14_103335_create_penerimaan_kas_table.php
+++ database/migrations/2026_04_14_103335_create_penerimaan_kas_table.php
@@ -8,19 +8,20 @@ return new class extends Migration
 {
     public function up(): void
     {
+        if (Schema::hasTable('penerimaan_kas')) {
+            return;
+        }
+
         Schema::create('penerimaan_kas', function (Blueprint $table) {
             $table->id('kode_terimakas');
-
             $table->unsignedBigInteger('kode_jual');
-
             $table->date('tgl_terimakas');
             $table->integer('jmlh_terimakas');
             $table->string('keterangan')->nullable();
-
             $table->timestamps();
 
-            $table->foreign('id_jual')
-                  ->references('id_jual')
+            $table->foreign('kode_jual')
+                  ->references('kode_jual')
                   ->on('penjualans')
                   ->cascadeOnDelete();
         });
